c8488ff568c10db9c2369ea97ba289630f108bae
Otavio Neves <92130086+otavionvs@users.noreply.github.com>
2022-11-08 19:19:34 -0300

    iniciando

---
+++ src/main/java/weg/com/Low/LowApplication.java
@@ -0,0 +1,13 @@
+package weg.com.Low;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/weg/com/Low/LowApplicationTests.java
@@ -0,0 +1,13 @@
+package weg.com.Low;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
