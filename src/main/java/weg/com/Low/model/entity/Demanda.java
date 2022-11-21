db4e247047ce2b5544d3d460c14ca8b066909c64
Otavio Neves <92130086+otavionvs@users.noreply.github.com>
2022-11-21 18:58:12 -0300

    sla

--- src/main/java/weg/com/Low/model/entity/Demanda.java
+++ src/main/java/weg/com/Low/model/entity/Demanda.java
@@ -1,6 +1,8 @@
 package weg.com.Low.model.entity;
 
 public class Demanda {
+    Integer codigoDemanda;
+    
     int idDemanda, idSolicitante, idAnalista, idGerenteDeNegocio;
     String tituloDemanda, statusDemanda, situacaoAtualDemanda, objetivoDemanda;
     String centroDeCustoDemanda, memoriaRealDemanda, memoriaPotencialDemanda, frequenciaDeUsoDemanda;
