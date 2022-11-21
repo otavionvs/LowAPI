eaf28367360e0fbfcbe1fed0aac2c3cfded0ab7b
Otavio Neves <92130086+otavionvs@users.noreply.github.com>
2022-11-21 19:03:34 -0300

    Create Usuario.java

---
+++ src/main/java/weg/com/Low/model/entity/Usuario.java
@@ -0,0 +1,22 @@
+package weg.com.Low.model.entity;
+
+public class Usuario {
+        int id;
+        String nome, departamento;
+
+        private void login(){
+
+        }
+        private void criarDemanda(){
+
+        }
+        private void salvarDemanda(){
+
+        }
+        private void enviarDemanda(){
+
+        }
+        private void verDamandas(){
+
+        }
+}
