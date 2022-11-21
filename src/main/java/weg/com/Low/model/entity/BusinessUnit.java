fbae4142ce07019b711eff6bdfdbe41be6d87440
viniciusz1 <vinicius101313@gmail.com>
2022-11-21 19:00:40 -0300

    codigo usuario

--- src/main/java/weg/com/Low/model/entity/BusinessUnit.java
+++ src/main/java/weg/com/Low/model/entity/BusinessUnit.java
@@ -1,4 +1,13 @@
 package weg.com.Low.model.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class BusinessUnit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private int idBussinessUnit;
+    @Column(nullable = false)
+    private String nome;
 }
