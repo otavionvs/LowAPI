137769838e0c03ca4276a8c41f815c83bed26065
Otavio Neves <92130086+otavionvs@users.noreply.github.com>
2022-11-21 21:17:09 -0300

    Estrutura Demanda

---
+++ src/main/java/weg/com/Low/model/entity/Beneficio.java
@@ -0,0 +1,22 @@
+package weg.com.Low.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "beneficio")
+@Data
+public class Beneficio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Integer codigoBeneficio;
+    @Column(nullable = false)
+    private String memoria_de_calculoBeneficio;
+    @Column(nullable = false)
+    private Double valorBeneficio;
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Moeda moedaBeneficio;
+}
--- src/main/java/weg/com/Low/model/entity/Status.java
+++ src/main/java/weg/com/Low/model/entity/Status.java
@@ -1,4 +1,19 @@
 package weg.com.Low.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum Status {
+    BackLog("BackLog"), //0
+    Assessment("Assessment"), //1
+    BusinessCase("Business Case"), //2
+    ToDo("To Do"), //3
+    DesignAndBuild("Design and Build"), //3
+    Support("Support"),
+    Cancelled("Cancelled"),
+    Done("Done"); //3
+
+    String status;
 }
