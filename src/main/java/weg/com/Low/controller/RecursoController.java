87a535beea26df3d3e8bdae5d87c94be2a055d7d
viniciusz1 <vinicius101313@gmail.com>
2022-12-07 15:55:49 -0300

    recurso clear

--- src/main/java/weg/com/Low/controller/RecursoController.java
+++ src/main/java/weg/com/Low/controller/RecursoController.java
@@ -6,17 +6,5 @@ import weg.com.Low.model.service.RecursoService;
 import java.util.List;
 
 public class RecursoController {
-    RecursoService recursoService;
-
-    public List<Recurso> findAll() {
-        return recursoService.findAll();
-    }
-
-    public  Recurso save(S entity) {
-        return recursoService.save(entity);
-    }
-
-    public void deleteById(Integer integer) {
-        recursoService.deleteById(integer);
-    }
+    
 }
