dae2ac6bb10a69ff8fc3a298c950359545753d6e
viniciusz1 <vinicius101313@gmail.com>
2022-12-19 15:37:21 -0300

    secao, post classificacao demanda

--- src/main/java/weg/com/Low/dto/SecaoDTO.java
+++ src/main/java/weg/com/Low/dto/SecaoDTO.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import javax.validation.constraints.NotBlank;
 
 @Getter
-public class SessaoDTO {
+public class SecaoDTO {
     @NotBlank
-    private String nomeSessao;
+    private String nomeSecao;
 }
