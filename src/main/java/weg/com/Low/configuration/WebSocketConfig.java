8d20b49c781a0b15646178e3a47802e547c17ed3
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2023-02-13 09:39:39 -0300

    .

--- src/main/java/weg/com/Low/configuration/WebSocketConfig.java
+++ src/main/java/weg/com/Low/configuration/WebSocketConfig.java
@@ -9,14 +9,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/all","/specific");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+         registry.addEndpoint("/ws");
+         registry.addEndpoint("/ws").withSockJS();
     }
 }
--- src/main/java/weg/com/Low/controller/DepartamentoController.java
+++ src/main/java/weg/com/Low/controller/DepartamentoController.java
@@ -2,28 +2,49 @@ package weg.com.Low.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import weg.com.Low.dto.DepartamentoDTO;
 import weg.com.Low.model.entity.Departamento;
 import weg.com.Low.model.service.DepartamentoService;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.Executors;
 
-//@CrossOrigin
+@CrossOrigin
 @AllArgsConstructor
 @Controller
 @RequestMapping("/departamento")
 public class DepartamentoController {
     private DepartamentoService departamentoService;
+    @Autowired
+    SimpMessagingTemplate simpMessagingTemplate;
+
 
     @GetMapping
     public ResponseEntity<List<Departamento>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(departamentoService.findAll());
+    }
+
+
+
+    @MessageMapping("/application")
+    @SendTo("/all/messages")
+    public Message send(final Message message) throws Exception {
+        return message;
     }
 
     @GetMapping("/{codigo}")
