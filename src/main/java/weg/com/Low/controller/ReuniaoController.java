048db87887a983e3e94ee58fb5a63a2ef7f538ce
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2023-04-27 16:33:34 -0300

    salvando versões

--- src/main/java/weg/com/Low/controller/ReuniaoController.java
+++ src/main/java/weg/com/Low/controller/ReuniaoController.java
@@ -111,12 +111,15 @@ public class ReuniaoController {
         ArrayList<Proposta> listaPropostas = new ArrayList<>();
         for (int i = 0; i < reuniaoDTO.getPropostasReuniao().size(); i++) {
             Proposta proposta = (Proposta) demandaService.findLastDemandaById(reuniaoDTO.getPropostasReuniao().get(i).getCodigoDemanda()).get();
-            proposta.setStatusDemanda(Status.DISCUSSION);
-            proposta.setVersion(proposta.getVersion() + 1);
-            //É criado uma nova proposta para atulizar a versão corretamente.
-            //Necessário para a realização de um PUT
+
             Proposta propostaNova = new Proposta();
             BeanUtils.copyProperties(proposta, propostaNova);
+
+
+            propostaNova.setStatusDemanda(Status.DISCUSSION);
+            propostaNova.setVersion(proposta.getVersion() + 1);
+            //É criado uma nova proposta para atulizar a versão corretamente.
+            //Necessário para a realização de um PUT
             listaPropostas.add(propostaService.save(propostaNova));
         }
 
@@ -176,14 +179,14 @@ public class ReuniaoController {
         for (Proposta proposta : reuniao.getPropostasReuniao()) {
             //Aqui deve retornar ao status anterior.
             if (proposta.getStatusDemanda() == Status.DISCUSSION) {
-                Proposta propostaAnterior = (Proposta) demandaService.findFirstByCodigoDemandaAndVersion(proposta.getCodigoDemanda(), proposta.getVersion() - 1).get();
-                System.out.println(propostaAnterior);
-                propostaAnterior.setVersion(proposta.getVersion() + 1);
+                Demanda propostaAnterior = demandaService.findFirstByCodigoDemandaAndVersion(proposta.getCodigoDemanda(), proposta.getVersion() - 1).get();
                 Proposta propostaNova = new Proposta();
                 BeanUtils.copyProperties(propostaAnterior, propostaNova);
+                propostaNova.setVersion(propostaAnterior.getVersion() + 2);
                 propostaService.save(propostaNova);
             }
         }
+
         return ResponseEntity.status(HttpStatus.OK).body(reuniaoService.save(reuniao));
     }
 
