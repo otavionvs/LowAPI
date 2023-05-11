ba8fa09f30c95149e320dbc0c7d440ddab850e15
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2023-05-11 16:43:38 -0300

    Atualizando status da reunião conforme a data

--- src/main/java/weg/com/Low/model/service/ReuniaoService.java
+++ src/main/java/weg/com/Low/model/service/ReuniaoService.java
@@ -2,24 +2,31 @@ package weg.com.Low.model.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import weg.com.Low.model.entity.*;
 import weg.com.Low.model.enums.StatusNotificacao;
+import weg.com.Low.model.enums.StatusReuniao;
 import weg.com.Low.model.enums.TipoNotificacao;
 import weg.com.Low.repository.ReuniaoRepository;
 
+import javax.annotation.PostConstruct;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @AllArgsConstructor
+@EnableScheduling
 public class ReuniaoService {
     private ReuniaoRepository reuniaoRepository;
     private NotificacaoService notificacaoService;
     private DemandaClassificadaService demandaClassificadaService;
     private DemandaService demandaService;
+
 
     public List<Reuniao> findAll() {
         return reuniaoRepository.findAll();
@@ -38,6 +45,31 @@ public class ReuniaoService {
 //                usuarios
 //                ));
         return reuniaoRepository.save(reuniao);
+    }
+    @PostConstruct
+    public void inicializar() {
+        atualizarStatusProximo();
+    }
+
+    //43200000 equivale a meio dia
+    @Scheduled(fixedDelay = 43200000)
+    public void atualizarStatusProximo() {
+        Date agora = new Date();
+        Date umaSemanaDepois = new Date(agora.getTime() + (7 * 24 * 60 * 60 * 1000));
+        // recupera todas as reuniões que podem ter seu status alterado
+        List<Reuniao> reunioes = reuniaoRepository.findByDataReuniaoBetweenAndStatusReuniao(agora, umaSemanaDepois, StatusReuniao.AGUARDANDO);
+
+        for (Reuniao reuniao : reunioes) {
+            reuniao.setStatusReuniao(StatusReuniao.PROXIMO);
+            reuniaoRepository.save(reuniao);
+        }
+
+
+        List<Reuniao> reunioesPendentes = reuniaoRepository.findByDataReuniaoBeforeAndStatusReuniao(agora, StatusReuniao.PROXIMO);
+        for (Reuniao reuniao : reunioesPendentes) {
+            reuniao.setStatusReuniao(StatusReuniao.PENDENTE);
+            reuniaoRepository.save(reuniao);
+        }
     }
 
     public Optional<Reuniao> findById(Integer codigo) {
--- src/main/java/weg/com/Low/util/DatabaseInitializer.java
+++ src/main/java/weg/com/Low/util/DatabaseInitializer.java
@@ -35,6 +35,7 @@ public class DatabaseInitializer implements CommandLineRunner {
 
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         Usuario usuario = usuarioRepository.save(new Usuario(1,"nomeUsuario", "gt", "gt", encoder.encode("gt"), departamento, NivelAcesso.GestorTI));
+        Usuario usuario2 = usuarioRepository.save(new Usuario(2,"nomeUsuario", "gt2", "gt2@", encoder.encode("gt2"), departamento, NivelAcesso.GestorTI));
 
         List<Demanda> listaDemandas = new ArrayList<>();
         for(int i = 0; i < 70; i++){
@@ -42,11 +43,12 @@ public class DatabaseInitializer implements CommandLineRunner {
         }
         listaDemandas = demandaRepository.saveAll(listaDemandas);
 
-        List<DemandaClassificada> listaDemandasClassificadas = new ArrayList<>();
-        for(int i = 0; i < 20; i++){
-            listaDemandasClassificadas.add(demandaClassificadaRepository.save(gerarDemandaClassificada(listaDemandas.get(i))));
-        }
-        listaDemandasClassificadas = demandaClassificadaRepository.saveAll(listaDemandasClassificadas);
+//        List<DemandaClassificada> listaDemandasClassificadas = new ArrayList<>();
+//        for(int i = 0; i < 20; i++){
+//            listaDemandasClassificadas.add(demandaClassificadaRepository.save(gerarDemandaClassificada(listaDemandas.get(i))));
+//        }
+//
+//        demandaClassificadaRepository.saveAll(listaDemandasClassificadas);
 
 //        List<Proposta> listaPropostas = new ArrayList<>();
 //        for(int i = 0; i < 10; i++){
@@ -64,6 +66,7 @@ public class DatabaseInitializer implements CommandLineRunner {
 
     public Proposta gerarProposta(DemandaClassificada demanda){
         Proposta proposta = new Proposta();
+        BeanUtils.copyProperties(demanda, proposta);
         proposta.setPrazoProposta(Faker.instance().date().birthday());
         proposta.setCodigoPPMProposta(12345);
         proposta.setJiraProposta(Faker.instance().lorem().characters(30));
@@ -71,8 +74,7 @@ public class DatabaseInitializer implements CommandLineRunner {
         proposta.setFimExDemandaProposta(Faker.instance().date().birthday());
         proposta.setPaybackProposta(Faker.instance().number().randomDouble(2,1, 50000));
         proposta.setEscopoDemandaProposta(Faker.instance().leagueOfLegends().summonerSpell());
-        BeanUtils.copyProperties(demanda, proposta);
-        proposta.setVersion(2);
+        proposta.setVersion(demanda.getVersion() + 1);
         proposta.setStatusDemanda(Status.ASSESSMENT);
         return proposta;
     }
