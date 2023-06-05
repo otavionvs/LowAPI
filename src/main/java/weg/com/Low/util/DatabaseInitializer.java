8613e8f2be26d24ff4a5c82ae47112f42161619d
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2023-06-05 09:31:52 -0300

    inserção de dados fake

--- src/main/java/weg/com/Low/util/DatabaseInitializer.java
+++ src/main/java/weg/com/Low/util/DatabaseInitializer.java
@@ -1,5 +1,7 @@
 package weg.com.Low.util;
+
 import com.github.javafaker.Faker;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,12 +10,16 @@ import org.springframework.stereotype.Component;
 import weg.com.Low.model.entity.*;
 import weg.com.Low.model.enums.*;
 import weg.com.Low.repository.*;
+
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Component
-public class DatabaseInitializer implements CommandLineRunner {
+public class DatabaseInitializer implements CommandLineRunner{
+    static ModelMapper modelMapper = new ModelMapper();
 
     @Autowired
     private DepartamentoRepository departamentoRepository;
@@ -27,84 +33,117 @@ public class DatabaseInitializer implements CommandLineRunner {
     private DemandaClassificadaRepository demandaClassificadaRepository;
     @Autowired
     private PropostaRepository propostaRepository;
+    @Autowired
+    private ReuniaoRepository reuniaoRepository;
 
-    @Override
-    public void run(String... args) throws Exception {
-        // Insere alguns dados no banco de dados
-//        Departamento departamento = departamentoRepository.save(new Departamento(1, "Departamento"));
-//
-//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//        Usuario usuario = usuarioRepository.save(new Usuario(1,"nomeUsuario", "gt", "gt", encoder.encode("gt"), departamento, NivelAcesso.GestorTI));
-//        Usuario usuario2 = usuarioRepository.save(new Usuario(2,"nomeUsuario", "gt2", "gt2@", encoder.encode("gt2"), departamento, NivelAcesso.GestorTI));
-//
-//        List<Demanda> listaDemandas = new ArrayList<>();
-//        for(int i = 0; i < 70; i++){
-//            listaDemandas.add(gerarDemanda(usuario, i));
-//        }
-//        listaDemandas = demandaRepository.saveAll(listaDemandas);
 
-//        List<DemandaClassificada> listaDemandasClassificadas = new ArrayList<>();
-//        for(int i = 0; i < 20; i++){
-//            listaDemandasClassificadas.add(demandaClassificadaRepository.save(gerarDemandaClassificada(listaDemandas.get(i))));
-//        }
-//
-//        demandaClassificadaRepository.saveAll(listaDemandasClassificadas);
-
-//        List<Proposta> listaPropostas = new ArrayList<>();
-//        for(int i = 0; i < 10; i++){
-//            listaPropostas.add(gerarProposta(listaDemandasClassificadas.get(i)));
-//        }
-//        propostaRepository.saveAll(listaPropostas);
-    }
-
-    public CentroCusto gerarCentroCusto(){
+    public CentroCusto gerarCentroCusto() {
         CentroCusto centroCusto = new CentroCusto();
         centroCusto.setNomeCentroCusto(Faker.instance().name().bloodGroup());
         centroCusto.setPorcentagemCentroCusto(100);
-        return  centroCusto;
+        return centroCusto;
     }
 
-    public Proposta gerarProposta(DemandaClassificada demanda){
-        Proposta proposta = new Proposta();
-        BeanUtils.copyProperties(demanda, proposta);
+    public Proposta gerarProposta(DemandaClassificada demanda) {
+        Proposta proposta = modelMapper.map(demanda, Proposta.class);
         proposta.setPrazoProposta(Faker.instance().date().birthday());
         proposta.setCodigoPPMProposta(12345);
         proposta.setJiraProposta(Faker.instance().lorem().characters(30));
         proposta.setInicioExDemandaProposta(Faker.instance().date().birthday());
         proposta.setFimExDemandaProposta(Faker.instance().date().birthday());
-        proposta.setPaybackProposta(Faker.instance().number().randomDouble(2,1, 50000));
+        proposta.setPaybackProposta(Faker.instance().number().randomDouble(2, 1, 50000));
         proposta.setEscopoDemandaProposta(Faker.instance().leagueOfLegends().summonerSpell());
         proposta.setVersion(demanda.getVersion() + 1);
         proposta.setStatusDemanda(Status.ASSESSMENT);
         return proposta;
     }
 
-    public DemandaClassificada gerarDemandaClassificada(Demanda demanda){
-        DemandaClassificada demandaClassificada = new DemandaClassificada();
+    public DemandaClassificada gerarDemandaClassificada(Demanda demanda) {
+        DemandaClassificada demandaClassificada = modelMapper.map(demanda, DemandaClassificada.class);
         demandaClassificada.setTamanhoDemandaClassificada(TamanhoDemanda.Grande);
         demandaClassificada.setBuSolicitanteDemandaClassificada(BussinessUnit.WAU);
         demandaClassificada.setBusBeneficiadasDemandaClassificada(List.of(BussinessUnit.WAU));
         demandaClassificada.setAnalista(demanda.getSolicitanteDemanda());
         demandaClassificada.setSecaoDemandaClassificada(Secao.AAS);
-        BeanUtils.copyProperties(demanda, demandaClassificada);
         demandaClassificada.setVersion(1);
         demandaClassificada.setStatusDemanda(Status.BACKLOG_PROPOSTA);
         return demandaClassificada;
     }
 
-    public Demanda gerarDemanda(Usuario usuario, Integer codigoDemanda){
+    public Demanda gerarDemanda(Usuario usuario, Integer codigoDemanda) {
         Demanda demanda = new Demanda();
         demanda.setCodigoDemanda(codigoDemanda);
         demanda.setVersion(0);
         demanda.setStatusDemanda(Status.BACKLOG_CLASSIFICACAO);
         demanda.setTituloDemanda(Faker.instance().name().title());
-        demanda.setSituacaoAtualDemanda(Faker.instance().lorem().sentence());
+        demanda.setSituacaoAtualDemanda(Faker.instance().gameOfThrones().quote());
         demanda.setObjetivoDemanda(Faker.instance().lorem().sentence());
         demanda.setFrequenciaDeUsoDemanda("MuitoAlta");
         demanda.setSolicitanteDemanda(usuario);
+
+        Beneficio beneficio = new Beneficio();
+
+        beneficio.setMoedaBeneficio(Moeda.Real);
+        beneficio.setMemoriaDeCalculoBeneficio(Faker.instance().lorem().fixedString(1000));
+        beneficio.setValorBeneficio(Faker.instance().number().randomDouble(5, 500, 50000000));
+
+        demanda.setBeneficioPotencialDemanda(beneficio);
+
+        Beneficio beneficio2 = new Beneficio();
+
+        beneficio2.setMoedaBeneficio(Moeda.Dollar);
+        beneficio2.setMemoriaDeCalculoBeneficio(Faker.instance().lorem().fixedString(1000));
+        beneficio2.setValorBeneficio(Faker.instance().number().randomDouble(5, 500, 50000000));
+
+        demanda.setBeneficioRealDemanda(beneficio2);
+
+
+        demanda.setBeneficioQualitativoDemanda(Faker.instance().lorem().fixedString(1000));
+
         List<CentroCusto> listaCentroCusto = new ArrayList<>();
         listaCentroCusto.add(gerarCentroCusto());
         centroCustoRepository.saveAll(listaCentroCusto);
         return demanda;
+    }
+    public Reuniao gerarReuniao(int codigoReuniao, List<Proposta> demandas){
+        Reuniao reuniao = new Reuniao();
+        reuniao.setDataReuniao(Faker.instance().date().future(3, TimeUnit.DAYS));
+        reuniao.setCodigoReuniao(codigoReuniao);
+        reuniao.setComissaoReuniao(Comissao.CPGCI);
+        reuniao.setStatusReuniao(StatusReuniao.PROXIMO);
+//        reuniao.setPropostasReuniao(demandas);
+        return reuniao;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Departamento departamento = departamentoRepository.save(new Departamento(1, "Departamento"));
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        List<Usuario> usuarios =  usuarioRepository.findAll();
+
+        if(usuarios.size() != 0){
+           return;
+        }
+
+        Usuario usuario = usuarioRepository.save(new Usuario(1, "nomeUsuario", "gt", "gt", encoder.encode("gt"), departamento, NivelAcesso.GestorTI));
+        Usuario usuario2 = usuarioRepository.save(new Usuario(2, "nomeUsuario", "gt2", "gt2@", encoder.encode("gt2"), departamento, NivelAcesso.GestorTI));
+
+        List<Proposta> propostas = new ArrayList<>();
+        for (int i = 1; i < 70; i++) {
+            Demanda demanda = demandaRepository.save(gerarDemanda(usuario, i));
+            if (i < 20) {
+                DemandaClassificada demandaClassificada = demandaClassificadaRepository.save(gerarDemandaClassificada(demanda));
+                if (i < 11) {
+                    Proposta proposta = propostaRepository.save(gerarProposta(demandaClassificada));
+                    propostas.add(proposta);
+                    if(i %2 == 0){
+                        Reuniao reuniao = gerarReuniao(i, propostas);
+                        reuniaoRepository.save(reuniao);
+                        propostas.clear();
+                    }
+                }
+            }
+        }
     }
 }
